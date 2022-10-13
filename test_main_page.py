463626b010d1b0434c5f7daf03c2286e0c08d501
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2022-10-13 16:37:24 +0800

    add main_page & rewrite test_main_page using POM

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(3)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_link_page()
